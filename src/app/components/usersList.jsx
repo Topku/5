784408aa011d282565b503bef2b4240880305942
Jaggmort <djeedo@gmail.com>
2022-04-15 22:29:04 +0300

    forms 1

--- src/app/components/usersList.jsx
+++ src/app/components/usersList.jsx
@@ -11,6 +11,7 @@ import _ from "lodash";
 const UsersList = ({ users: allUsers, ...rest }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [professions, setProfessions] = useState();
+  const [searchQuery, setSearchQuery] = useState("");
   const [selectedProf, setSelectedProf] = useState();
   const [sortBy, setSortBy] = useState({
     path: "name",
@@ -41,9 +42,14 @@ const UsersList = ({ users: allUsers, ...rest }) => {
   }, []);
   useEffect(() => {
     setCurrentPage(1);
-  }, [selectedProf]);
+  }, [selectedProf, searchQuery]);
   const handleProfessionSelect = (item) => {
+    if (searchQuery !== "") setSearchQuery("");
     setSelectedProf(item);
+  };
+  const handleSearchQuery = ({ target }) => {
+    setSelectedProf(undefined);
+    setSearchQuery(target.value);
   };
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex);
@@ -53,9 +59,15 @@ const UsersList = ({ users: allUsers, ...rest }) => {
   };
 
   if (users) {
-    const filteredUsers = selectedProf
+    const filteredUsers = searchQuery
+      ? users.filter(
+          (user) =>
+            user.name.toLowerCase().includes(searchQuery.toLowerCase()) === true
+        )
+      : selectedProf
       ? users.filter((user) => user.profession._id === selectedProf._id)
       : users;
+    console.log(searchQuery);
     const count = filteredUsers.length;
     const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
     const usersCrop = paginate(sortedUsers, currentPage, pageSize);
@@ -80,6 +92,13 @@ const UsersList = ({ users: allUsers, ...rest }) => {
         )}
         <div className="d-flex flex-column">
           <SearchStatus length={count} />
+          <input
+            type="text"
+            name="searchQuery"
+            placeholder="Search..."
+            onChange={handleSearchQuery}
+            value={searchQuery}
+          />
           {count > 0 && (
             <UsersTable
               users={usersCrop}
